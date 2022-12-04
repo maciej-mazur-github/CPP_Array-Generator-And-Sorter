931a17f343c062d1d37eae2760f80bf7f39af70f
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-12-04 13:29:36 +0100

    Remove reduntant class and method declarations from .cpp file

--- Array Generator And Sorter/array_generator_and_sorter.cpp
+++ Array Generator And Sorter/array_generator_and_sorter.cpp
@@ -2,25 +2,9 @@
 #include <iostream>
 #include <ctime>
 #include <utility>
+#include "array_generator_and_sorter.h"
 
 using namespace std;
-
-class ArrayGeneratorSorter
-{
-public:
-	static void userArrayManualFillIn(int arraySize, int arr[]);
-	static void userArrayAutomaticFillIn(int arraySize, int arr[]);
-	static int* generateArrayManualFillIn(int arraySize);
-	static int* generateArrayAutomaticFillIn(int arraySize);
-	static void sortingAlgoritmChoice(int arraySize, int arr[]);
-	static void printArray(int arraySize, int arr[]);
-private:
-	static void printSortingOptions();
-	static int minElementIndex(int startIndex, int arraySize, int arr[]);
-	static void bubbleSort(int arraySize, int arr[]);
-	static void selectionSort(int arraySize, int arr[]);
-	static void insertionSort(int arraySize, int arr[]);
-};
 
 
 
